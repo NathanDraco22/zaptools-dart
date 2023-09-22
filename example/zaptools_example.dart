1f23aa17d3c1c6b4fd139d08b9afb652165054f1
NathanDraco22 <nathandraco22@gmail.com>
2023-09-21 21:39:53 -0600

    example updated

--- example/zaptools_example.dart
+++ example/zaptools_example.dart
@@ -3,20 +3,7 @@ import 'package:zaptools/zaptools_client.dart';
 
 void main() {
 
-  Uri uri = Uri.parse("ws://127.0.0.1:3000/ws");
-  final zapClient = ClientConnector.connect(uri);
-
-  zapClient.onConnected((eventData) {
-    print("client connected oh yeah!");
-   });
-
-  zapClient.onDisconnected((eventData) {
-    print("Cliente disconnected bye bye");
-  });
-
-  zapClient.sendEvent("hola", "payload");
-
-  zapClient.onEvent("saludo", print);
+subcribersDemo();
   
 }
 
@@ -31,10 +18,26 @@ void subcribersDemo(){
       zapClient.sendEvent("hola", "soy el cliente alv~~");
     }
     if (event case ConnectionState.offline) {
-      print("disconnected");
-      ClientConnector.tryReconnect(zapClient);
+      print("disconnected   bye bye");
     }
   });
 
   zapClient.subscribeToEvent("saludo").listen(print);
+}
+
+void callBackDemo(){
+  Uri uri = Uri.parse("ws://127.0.0.1:3000/ws");
+  final zapClient = ClientConnector.connect(uri);
+
+  zapClient.onConnected((eventData) {
+    print("client connected oh yeah!");
+   });
+
+  zapClient.onDisconnected((eventData) {
+    print("Cliente disconnected bye bye");
+  });
+
+  zapClient.sendEvent("hola", "payload");
+
+  zapClient.onEvent("saludo", print);
 }
