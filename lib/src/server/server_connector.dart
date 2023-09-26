363e9d20683de15f881bcd1bbeef5bee74156e1b
NathanDraco22 <nathandraco22@gmail.com>
2023-09-26 13:12:13 -0600

    id strategy added

--- lib/src/server/server_connector.dart
+++ lib/src/server/server_connector.dart
@@ -1,16 +1,25 @@
 import 'dart:io';
 
+import 'package:zaptools/src/server/id_controller.dart';
+
 import 'adapter/adapters.dart';
 import 'event_processor.dart';
 import 'websocket_connection.dart';
 import 'event_tools_server.dart';
 
 
-Future<void> plugAndStartWithIO(HttpRequest request, EventRegister eventRegister) async{
+Future<void> plugAndStartWithIO(
+  HttpRequest request, 
+  EventRegister eventRegister,
+  {
+    ZapIdStrategy idStrategy = const DefaultIDStrategy()
+  }
+) async{
     final websocket = await WebSocketTransformer.upgrade(request);
     final adapter = IOadapter(websocket);
     final eventCaller = EventCaller(eventRegister.eventBook);
-    final wsConn = WebSocketConnection("1", adapter);
+    final id = idStrategy.eval();
+    final wsConn = WebSocketConnection(id, adapter);
     final proccesor = EventProcessor(eventCaller, wsConn);
     proccesor.notifyConnected();
     websocket.listen(
