ae1957cf98631ed1ee3b5c997a3c7730e76f0358
NathanDraco22 <nathandraco22@gmail.com>
2023-11-20 20:58:21 -0600

    updated logs

--- packages/zaptools_client/lib/src/client/consumer/zapclient.dart
+++ packages/zaptools_client/lib/src/client/consumer/zapclient.dart
@@ -40,7 +40,7 @@ class ZapClient extends ZapConsumer {
     final eventInvoker = EventInvoker(_eventBook);
     _shareConnectionState(ConnectionState.online);
     eventInvoker.invoke(EventData("connected", {}, {}));
-    log("Connected", name: "ZapClient");
+    log("Online", name: "ZapClient");
     _subscription = _connectionStream.listen(
         (data) {
           final eventData = Validators.convertAndValidate(data);
@@ -50,7 +50,7 @@ class ZapClient extends ZapConsumer {
         onDone: () {
           _shareConnectionState(ConnectionState.offline);
           eventInvoker.invoke(EventData("disconnected", {}, {}));
-          log("Disconnected", name: "ZapClient");
+          log("Offline", name: "ZapClient");
           _subscription?.cancel();
         });
   }
@@ -94,7 +94,7 @@ class ZapSubscriber extends ZapConsumer {
   Future<void> start() async {
     await _channelSession.ready;
     Future(() => _shareConnectionState(ConnectionState.online));
-    log("Connected", name: "ZapSubscriber");
+    log("Online", name: "ZapSubscriber");
     _subscription = _connectionStream.listen(
         (data) {
           final eventData = Validators.convertAndValidate(data);
@@ -103,7 +103,7 @@ class ZapSubscriber extends ZapConsumer {
         cancelOnError: true,
         onDone: () {
           _shareConnectionState(ConnectionState.offline);
-          log("Connected", name: "ZapSubscriber");
+          log("Offline", name: "ZapSubscriber");
           _subscription?.cancel();
         });
   }
@@ -113,111 +113,3 @@ class ZapSubscriber extends ZapConsumer {
     await _channelSession.close();
   }
 }
-
-
-// class ZapClient {
-//   ChannelSession _channelSession;
-
-//   Stream _connectionStream;
-//   WebSocketSink _webSocketSink;
-//   final EventBook _eventBook;
-//   StreamSubscription? _subscription;
-
-//   Function(ConnectionState state)? _connectionListener;
-
-//   final StreamController<EventData> _streamController =
-//       StreamController<EventData>.broadcast();
-
-//   final ConnectionStateNotifier _connectionStateNotifier =
-//       ConnectionStateNotifier();
-
-//   Stream<ConnectionState> get connectionState =>
-//       _connectionStateNotifier.stream;
-
-//   ZapClient(ChannelSession webSocketSession, {required EventBook eventBook})
-//       : _eventBook = eventBook,
-//         _webSocketSink = webSocketSession.channel.sink,
-//         _connectionStream = webSocketSession.channel.stream,
-//         _channelSession = webSocketSession;
-
-//   Uri get uri => _channelSession.uri;
-
-//   void onConnectionStateChanged(Function(ConnectionState state) callback) {
-//     _connectionListener = callback;
-//   }
-
-//   void onEvent(String eventName, EventCallback callback) {
-//     _eventBook.saveEvent(Event(eventName, callback));
-//   }
-
-//   void sendEvent(String eventName, dynamic payload,
-//       {Map<String, dynamic>? headers}) {
-//     final data = {
-//       "headers": headers ?? {},
-//       "eventName": eventName,
-//       "payload": payload
-//     };
-//     final jsonString = json.encode(data);
-//     try {
-//       _webSocketSink.add(jsonString);
-//     } catch (e) {
-//       log("Connection Closed unable to send event", level: 800);
-//     }
-//   }
-
-//   void _invokeEvent(EventData eventData) {
-//     final eventName = eventData.name;
-//     final event = _eventBook.eventRecords[eventName];
-//     if (event == null) return;
-//     event.callback(eventData);
-//   }
-
-//   void start() async {
-//     await _channelSession.channel.ready;
-//     _shareConnectionState(ConnectionState.online);
-//     _subscription = _connectionStream.listen(
-//         (data) {
-//           final eventData = Validators.convertAndValidate(data);
-//           _invokeEvent(eventData);
-//           _streamController.add(eventData);
-//         },
-//         cancelOnError: true,
-//         onDone: () {
-//           _shareConnectionState(ConnectionState.offline);
-//           _subscription?.cancel();
-//           _webSocketSink.close();
-//         });
-//   }
-
-//   void _updateSession(ChannelSession webSocketSession) {
-//     _channelSession = webSocketSession;
-//     _webSocketSink = webSocketSession.channel.sink;
-//     _connectionStream = webSocketSession.channel.stream;
-//   }
-
-//   void _shareConnectionState(ConnectionState state) {
-//     if (_connectionListener != null) {
-//       _connectionListener!(state);
-//     }
-//     _connectionStateNotifier.emit(state);
-//   }
-
-//   Stream<EventData> subscribeToEvent(String eventName) =>
-//       _streamController.stream.where((event) => event.name == eventName);
-
-//   Stream<EventData> subscribeToAllEvent() => _streamController.stream;
-
-//   Stream<EventData> subscribeToEvents(List<String> eventNames) {
-//     return _streamController.stream
-//         .where((event) => eventNames.contains(event.name));
-//   }
-
-//   Future<void> disconnect() async {
-//     await _webSocketSink.close();
-//   }
-
-//   Future<void> clean() async {
-//     await _streamController.close();
-//     await _webSocketSink.close();
-//   }
-// }
--- packages/zaptools_client/lib/zaptools_client.dart
+++ packages/zaptools_client/lib/zaptools_client.dart
@@ -5,5 +5,6 @@ library zaptools_client;
 
 export 'src/client/connection_state.dart';
 export 'src/client/consumer/client_connector.dart';
+export 'src/shared/event_tools.dart' show EventData;
 
 // TODO: Export any libraries intended for clients of this package.
