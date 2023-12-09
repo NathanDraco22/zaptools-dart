091f9c92ecd4623c740c5d47b2fb32b1943f7279
NathanDraco22 <nathandraco22@gmail.com>
2023-12-08 20:23:52 -0600

    context modified

--- packages/zaptools_server/lib/src/server/tools/event_context.dart
+++ packages/zaptools_server/lib/src/server/tools/event_context.dart
@@ -2,7 +2,14 @@ import '../../shared/event_tools.dart';
 import '../websocket_connection.dart';
 
 class EventContext {
+  EventContext( this.eventData, this.connection):
+  eventName = eventData.name,
+  payload = eventData.payload,
+  headers = eventData.headers;
+
   EventData eventData;
   WebSocketConnection connection;
-  EventContext(this.eventData, this.connection);
+  String eventName;
+  dynamic payload;
+  Map<String, dynamic> headers;
 }
