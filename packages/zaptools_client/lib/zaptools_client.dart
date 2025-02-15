8cfe3bac959806023686478f6e81ae28a1d52571
NathanDraco22 <nathandraco22@gmail.com>
2025-02-15 12:03:44 -0600

    removed unncessary subscriber

--- packages/zaptools_client/lib/zaptools_client.dart
+++ packages/zaptools_client/lib/zaptools_client.dart
@@ -4,7 +4,6 @@
 library zaptools_client;
 
 export 'src/client/consumer/zapclient.dart' show ZapClient;
-export 'src/client/consumer/zapsubscriber.dart' show ZapSubscriber;
 export 'src/client/consumer/zapconsumer.dart' show ZapConsumer;
 export 'src/client/connection_state.dart' show ZapClientState;
 export 'src/shared/event_tools.dart' show EventData;
